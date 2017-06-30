bd940110a68b72e3ca4c8d8e0295840cdb7a5647
Matthew Gregory Krupa <mgkrupa@gmail.com>
2017-06-30 15:50:59 -0700

    Update

--- merge_without_buffer.h
+++ merge_without_buffer.h
@@ -1,19 +1,18 @@
 /*
- * merge_without_buffer_trim2.h
+ * merge_without_buffer.h
  *
- *  Created on: Jun 30, 2017
+ *  Created on: Jun 29, 2017
  *      Author: Matthew Gregory Krupa
  *
- *  A simplified version of MergeWithOutBuffer() that is faster but more
- *   complicated than MergeWithOutBufferTrim 2().
+ *  The most important function defined in this file is MergeWithOutBuffer().
  *  MergeWithOutBufferTrim1() contains the near minimum code needed to make the
  *   merge algorithm work.
- *  It is also the slowest of MergeWithOutBuffer(), MergeWithOutBufferTrim 2(),
+ *  It is the fastest of MergeWithOutBuffer(), MergeWithOutBufferTrim 2(),
  *   and MergeWithOutBufferTrim1().
  */
 
-#ifndef SRC_MERGE_WITHOUT_BUFFER_TRIM2_H_
-#define SRC_MERGE_WITHOUT_BUFFER_TRIM2_H_
+#ifndef SRC_MERGE_WITHOUT_BUFFER_H_
+#define SRC_MERGE_WITHOUT_BUFFER_H_
 
 #include <algorithm>
 
@@ -45,6 +44,11 @@
  * If at any point any one of the subintervals becomes <= 2 in length then
  *  it will return.
  *
+ * If after execution completes both subranges have size >= 3, (i.e.
+ *  start_right - start_left >= 3 and end_left + 1 - start_right >= 3)
+ *  then it is guaranteed that:
+ *  1) *start_left > *(start_right + 2)
+ *  2) *end_right  < *(end_left - 2)
  * If after execution completes both subranges have size >= 2, then it is
  *  guaranteed that:
  *  1) *start_left > *(start_right + 1)
@@ -60,7 +64,8 @@
  *   start_right < end_right).
  */
 template<class T>
-void TrimEnds2(T &start_left_out, T &end_left_out, T &start_right_out, T &end_right_out) {
+void TrimEnds3(T &start_left_out,  T &end_left_out,
+               T &start_right_out, T &end_right_out) {
   auto start_left  = start_left_out,  end_left  = end_left_out;
   auto start_right = start_right_out, end_right = end_right_out;
   bool is_trivial = false;
@@ -72,14 +77,15 @@ void TrimEnds2(T &start_left_out, T &end_left_out, T &start_right_out, T &end_ri
       end_right_out    = end_right;
       return ;
     }
-    if (*start_left <= *start_right) {//If true, then this implies that start_left < end_left
-      start_left = SmallestIteratorWithValueGreaterThan_KnownToExist(start_left + 1, end_left, *start_right);
-    }
-    if (*end_right >= *end_left) {
-      end_right = LargestIteratorWithValueLessThan_KnownToExist(start_right, end_right - 1, *end_left);
-    }
-    if (*start_left >= *end_right
-        || start_left >= end_left || start_right >= end_right) {
+    //If true, then this implies that start_left < end_left
+    if (*start_left <= *start_right)
+      start_left = SmallestIteratorWithValueGreaterThan_KnownToExist(
+                                       start_left + 1, end_left, *start_right);
+    if (*end_right >= *end_left)
+      end_right = LargestIteratorWithValueLessThan_KnownToExist(start_right,
+                                                     end_right - 1, *end_left);
+    if (*start_left >= *end_right || start_left >= end_left
+        || start_right >= end_right) {
       is_trivial = true;
       break;
     }
@@ -87,43 +93,133 @@ void TrimEnds2(T &start_left_out, T &end_left_out, T &start_right_out, T &end_ri
     // 1) both length_left and length_right are >= 2, and
     // 2) *end_left > *end_right > *start_left > *start_right.
 
-//NOTE/REMINDER: Up to 10 - 20% of elements are sometimes emplaced by the code inbetween this comment and the just_after_nested_loops label below. Also the first two main loops (i.e. the loops BEFORE while(*(end_right - 1) >= *(end_left - 2)) {...}) combined do about 2/3 of this total work.
-    //The two main while() loops after this if statement require that both the left and right vectors have at least elements 2 elements
-    if (*start_left <= *(start_right + 1)) { //Since start_right + 1 <= end_right, this is within bounds.
+    //NOTE/REMINDER: Up to 10 - 20% of elements are sometimes emplaced by the
+    // code in between this comment and the just_after_nested_loops label below.
+    //Also the first two main loops (i.e. the loops BEFORE
+    // while(*(end_right - 1) >= *(end_left - 2)) {...}) combined do about
+    // 2/3 of this total work.
+    //The two main while() loops after this if statement require that both the
+    // left and right vectors have at least elements 2 elements.
+    //
+    //Since start_right + 1 <= end_right, this is within bounds.
+    if (*start_left <= *(start_right + 1)) {
       do {
-        std::swap(*(start_left++), *start_right); //Both vectors will remain non-decreasing after this swap
-      } while (*start_left <= *(start_right + 1)) ;//start_left <= end_left since *end_left > *end_right >= *(start_right + 1)
-      //At this point, *start_left > *start_right since *start_left > *(start_right + 1) >= *start_right
+        //Both vectors will remain non-decreasing after this swap.
+        std::swap(*(start_left++), *start_right);
+      } while (*start_left <= *(start_right + 1)) ;//start_left <= end_left
+                           //since *end_left > *end_right >= *(start_right + 1).
+      //At this point, *start_left > *start_right since
+      // *start_left > *(start_right + 1) >= *start_right.
       if (start_left >= end_left || *start_left >= *end_right) {
         is_trivial = true;
         break;
       }
-      //At this point, *start_left > *(start_right + 1) and vec[start_left : end_left] has at least 2 elements
+      //At this point, *start_left > *(start_right + 1) and
+      // [start_left : end_left] has at least 2 elements.
     }
 
-    if (*(end_left - 1) <= *end_right) { //*(end_left - 1) is well-defined since start_left < end_left.
-      do {//Note end_right >= start_right + 1 since *end_left >= *(end_left - 1) >= *start_left > *(start_right + 1) >= *start_right
+    //*(end_left - 1) is well-defined since start_left < end_left.
+    if (*(end_left - 1) <= *end_right) {
+      do {//Note end_right >= start_right + 1 since *end_left >= *(end_left - 1)
+                        // >= *start_left > *(start_right + 1) >= *start_right.
         std::swap(*(end_right--), *end_left);
       } while (*(end_left - 1) <= *end_right) ;
       if (start_right >= end_right || *start_left >= *end_right) {
         is_trivial = true;
         break;
       }
-      //Note end_right > start_right + 1 since *end_right > *start_left > *(start_right + 1) and [start_right : end_right] is non-decreasing.
+      //Note end_right > start_right + 1 since *end_right > *start_left
+      //  > *(start_right + 1) and [start_right : end_right] is non-decreasing.
     }
     //At this point, *(end_left - 1) > *end_right
 
+    if (*(end_left - 2) <= *(end_right - 1)) {
+      do {
+        std::swap(*(end_left - 1), *(end_right - 1));
+        std::swap(*end_left, *end_right);
+        end_right -= 2;
+      } while (*(end_left - 2) <= *(end_right - 1)) ;
+      if (*(end_left - 1) <= *end_right)
+        std::swap(*end_left, *(end_right--));
+      if (*start_left >= *end_right) {
+        is_trivial = true;
+        break;
+      }
+    }
+
+    if(*(end_left - 2) <= *end_right) {//This implies that *end_right >=
+                                       // *(end_left - 2) > *(end_right - 1).
+      auto temp       = *end_left;
+      *end_left       = *(end_left - 1);
+      *(end_left - 1) = *end_right;
+      *end_right      = temp;
+      --end_right;
+      if (*start_left >= *end_right) {
+        is_trivial = true;
+        break;
+      }
+    }
+
+    if (*(start_left + 1) <= *(start_right + 2)) {
+      //In this case, our vector is something like:
+      // 2 2 2 2 2 3 4 4 9 ...1000 0 1 4 6... 500, which we can efficiently
+      // handle.
+      do {
+        std::swap(*(start_left + 1), *(start_right + 1));
+        std::swap(*start_left, *start_right);
+        start_left += 2;
+        //At this point, *start_right <= *(start_right + 1) <= *start_left.
+        //Note that *start_left==*start_right necessitates the below condition.
+        if (*start_left == *(start_right + 1)) {
+          //Note that initially, it's possible that *start_right < *start_left.
+          std::swap(*(start_left++), *start_right);
+          //At this point, *start_left >= *start_right == *(start_right + 1).
+          //This loop make sure that *start_left > *start_right.
+          while (*start_left == *start_right)
+            ++start_left; //start_left will always be <= end_left.
+        }
+        //At this point, *start_left > *(start_right + 1) >= *start_right and
+        // the right subvector's length is unchanged
+
+        //Upon reaching this point the first time, our example
+        // 2 2 2 2 2 3 4 4 9 ...1000 0 1 4 6... 500
+        // has become: 3 4 4 9 ...1000 2 2 4 6... 500
+      } while (*(start_left + 1) <= *(start_right + 2));
+      //At this point our example 2 2 2 2 2 3 4 4 9 ...1000 0 1 4 6... 500
+      // has become: 4 9 ...1000 3 4 4 6... 500
+      if (*start_left == *(start_right + 1)) {
+        std::swap(*(start_left++), *start_right);
+      }
+      if (*start_left >= *end_right) {
+        is_trivial = true;
+        break;
+      }
+    }
+
+    if (*start_left <= *(start_right + 2)) {
+      auto temp          = *start_left;
+      *start_left        = *start_right;
+      *start_right       = *(start_right + 1);
+      *(start_right + 1)   = temp;
+      ++start_left;
+      if (*start_left >= *end_right) {
+        is_trivial = true;
+        break;
+      }
+    }
     auto length_left  = std::distance(start_left, end_left + 1);
     auto length_right = std::distance(start_right, end_right + 1);
-    if (length_left <= length_right && *start_left >= *(start_right + length_left - 1)) {
+    if (length_left <= length_right && *start_left >=
+                                            *(start_right + length_left - 1)) {
       std::swap_ranges(start_left, end_left + 1, start_right);
       start_left   = start_right;
       start_right += length_left;
       end_left    += length_left;
       continue ;
     }
-    if (length_left >= length_right && *(end_left - (length_right - 1)) >= *end_right) {
-      std::swap_ranges(start_right, end_right + 1, end_left - (length_right - 1));
+    if (length_left >= length_right && *(end_left - (length_right - 1)) >=
+                                                                  *end_right) {
+      std::swap_ranges(start_right, end_right + 1, end_left - (length_right-1));
       end_left   -= length_right;
       start_right = end_left + 1;
       end_right   = start_right + (length_right - 1);
@@ -133,7 +229,8 @@ void TrimEnds2(T &start_left_out, T &end_left_out, T &start_right_out, T &end_ri
   }
 
   if (is_trivial) {
-    MergeTrivialCases(start_left, end_left, start_right, end_right, &start_left_out, &end_right_out);
+    MergeTrivialCases(start_left, end_left, start_right, end_right,
+                      &start_left_out, &end_right_out);
     start_left_out   = end_left + 1;
     end_left_out     = end_left;
     start_right_out  = end_right + 1;
@@ -147,17 +244,18 @@ void TrimEnds2(T &start_left_out, T &end_left_out, T &start_right_out, T &end_ri
   return ;
 }
 
-
 //Assumes that start_left <= start_right
 template<class RAI>
-void MergeWithOutBufferTrim2(RAI start_left, RAI end_left, RAI start_right, RAI end_right) {
+void MergeWithOutBuffer(RAI start_left,  RAI end_left,
+                        RAI start_right, RAI end_right) {
   int length_left, length_right, length_smaller, d;
-  TrimEnds2(start_left, end_left, start_right, end_right);
+  TrimEnds3(start_left, end_left, start_right, end_right);
   length_left  = std::distance(start_left, end_left + 1);
   length_right = std::distance(start_right, end_right + 1);
   length_smaller = length_left < length_right ? length_left : length_right;
   //Check for triviality.
-  if (start_left > end_left || start_right > end_right || *end_left <= *start_right)
+  if (start_left > end_left || start_right > end_right
+      || *end_left <= *start_right)
     return ;
   if (length_smaller <= 1) {
     if (length_smaller <= 0)
@@ -168,14 +266,18 @@ void MergeWithOutBufferTrim2(RAI start_left, RAI end_left, RAI start_right, RAI 
       return ;
     }
   }
-  d = DisplacementFromMiddleIiteratorToPotentialMediansContiguous_KnownToExist(end_left, length_smaller);
+  //At this point we're guaranteed to have start_left < start_right and
+  // *start_left > *start_right.
+  d = DisplacementFromMiddleIiteratorToPotentialMediansContiguous_KnownToExist(
+                                                      end_left, length_smaller);
   auto start_2nd_quarter = end_left - (d - 1);
   std::swap_ranges(start_2nd_quarter, end_left + 1, start_right);
   auto start_4th_quarter = start_right + d;
-  MergeWithOutBufferTrim2(start_left, start_2nd_quarter - 1, start_2nd_quarter, end_left);
-  MergeWithOutBufferTrim2(start_right, start_4th_quarter - 1, start_4th_quarter, end_right);
+  MergeWithOutBuffer(start_left, start_2nd_quarter - 1, start_2nd_quarter,
+                     end_left);
+  MergeWithOutBuffer(start_right, start_4th_quarter - 1, start_4th_quarter,
+                     end_right);
   return ;
 }
 
-
-#endif /* SRC_MERGE_WITHOUT_BUFFER_TRIM2_H_ */
+#endif /* SRC_MERGE_WITHOUT_BUFFER_H_ */
